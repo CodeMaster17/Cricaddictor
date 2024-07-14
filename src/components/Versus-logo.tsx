69235046d8652d4c5f928f3a32290d62bee2038c
Harshit Yadav <2105891@kiit.ac.in>
2024-07-15 00:28:39 +0530

    refactor: versus logo component

---
+++ src/components/Versus-logo.tsx
@@ -0,0 +1,10 @@
+
+const VersusLogo = () => {
+    return (
+        <div>
+            <img src="/versus.png" alt="versus" className="w-16 h-16" />
+        </div>
+    )
+}
+
+export default VersusLogo
