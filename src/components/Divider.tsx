7aeaafa48e40af595b6056f4d8db4ba8a5b3e02a
NaturesClub <2105891@kiit.ac.in>
2024-08-04 22:10:06 +0530

    feat: divider component created

---
+++ src/components/Divider.tsx
@@ -0,0 +1,12 @@
+
+const Divider = () => {
+    return (
+        <div className="w-full flex justify-between items-center mt-4">
+            <div className="w-[45%] border-[1px] border-black"></div>
+            <div className="w-[2px] rounded-full border-2 border-black"></div>
+            <div className="w-[45%] border-[1px] border-black"></div>
+        </div>
+    )
+}
+
+export default Divider
