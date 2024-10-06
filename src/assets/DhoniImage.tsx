a738a9c81251387b05e35dc418f35b13a184a7d0
Harshit Yadav <2105891@kiit.ac.in>
2024-10-06 11:48:30 +0530

    fix: Made dhoni image responsive

--- src/assets/DhoniImage.tsx
+++ src/assets/DhoniImage.tsx
@@ -2,7 +2,8 @@
 
 const DhoniImage = () => {
     return (
-        <img src="/dhoni.png" alt="hero-img" className="absolute bottom-0" />
+        <img src="/dhoni.png" alt="hero-img" className="absolute bottom-0 w-full max-w-[357px] h-auto sm:max-w-[320px] md:max-w-[400px] lg:max-w-[450px] xl:max-w-[500px]"
+        />
     )
 }
 
